6e4e622a39fd9b2b549f0b927ec90e0fbd96e726
Joel Grande <joel.grande@gruposhift-f6.com.mx>
2019-03-25 14:06:26 -0600

    Se agrego las clases para la entidad TipoMiscelaneo

--- src/mx/shf6/produccion/model/dao/TipoMiscelaneoDAO.java
+++ src/mx/shf6/produccion/model/dao/TipoMiscelaneoDAO.java
@@ -7,121 +7,127 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import mx.shf6.produccion.model.TipoMiscelaneo;
 import mx.shf6.produccion.utilities.Notificacion;
 
-public class TipoMiscelaneoDAO implements ObjectDAO {
+public class TipoMiscelaneoDAO {
 	
-	//METODO PARA HACER CREATE EN LA TABLA TIPOMISCELANEO
-	@Override
-	public boolean crear(Connection connection, Object TipoMiscelaneo) {	
-		TipoMiscelaneo tipoMiscelaneo = (TipoMiscelaneo) TipoMiscelaneo;
-		String query = "INSERT INTO tipoMiscelaneo (codigo, descripcion) "
-				+ "values ( ?, ?)";
-		try {	
-			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
-			preparedStatement.setString(1, tipoMiscelaneo.getCodigo());
-			preparedStatement.setString(2, tipoMiscelaneo.getDescripcion());
-			preparedStatement.execute();
-			return true;   
+	//METODO PARA CREAR UN REGISTRO
+	public static boolean createTipoMiscelaneo(Connection connection, TipoMiscelaneo tipoMiscelaneo) {
+		String consulta = "INSERT INTO tipoMiscelaneo (Codigo, Descripcion, Status) VALUES (?, ?, ?)";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setString(1, tipoMiscelaneo.getCodigo());
+			sentenciaPreparada.setString(2, tipoMiscelaneo.getDescripcion());
+			sentenciaPreparada.setInt(3,  tipoMiscelaneo.getStatusFK());
+			sentenciaPreparada.execute();
+			return true;
 		} catch (SQLException ex) {
 			Notificacion.dialogoException(ex);
-			return false;			
-		}//FIN TRY/CATCH
-	}//FIN METODO	
-	
-	//METODO PARA HACER SELECT EN LA TABLA TIPOMISCELANEO
-	@Override
-	public ArrayList<Object> leer(Connection connection, String campoBusqueda, String valorBusqueda) {
-		String query = "";
-		ArrayList<Object> listaTipoMiscelaneo = new ArrayList<Object>();
-		if (campoBusqueda.isEmpty() || valorBusqueda.isEmpty()) {
-			query = "SELECT * FROM tipoMiscelaneo ORDER BY sysPK;";
-			try {
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery(query);  
-				TipoMiscelaneo tipoMiscelaneo = null;
-				while (resultSet.next()) {
-					tipoMiscelaneo = new TipoMiscelaneo();
-					tipoMiscelaneo.setSysPk(resultSet.getInt(1));
-					tipoMiscelaneo.setCodigo(resultSet.getString(2));
-					tipoMiscelaneo.setDescripcion(resultSet.getString(3));
-					listaTipoMiscelaneo.add(tipoMiscelaneo);
-				}//FIN WHILE
-			} catch (SQLException ex) {
-				Notificacion.dialogoException(ex);
-			}//FIN TRY/CATCH
-		} else {
-			query = "SELECT * FROM tipoMiscelaneo WHERE " + campoBusqueda + " = ? ORDER BY sysPK;";
-			try {
-				PreparedStatement preparedStatement = connection.prepareStatement(query);
-				preparedStatement.setString(1, valorBusqueda);
-				ResultSet resultSet=preparedStatement.executeQuery();
-				TipoMiscelaneo tipoMiscelaneo = null;
-				while (resultSet.next()) {
-					tipoMiscelaneo = new TipoMiscelaneo();
-					tipoMiscelaneo.setSysPk(resultSet.getInt(1));
-					tipoMiscelaneo.setCodigo(resultSet.getString(2));
-					tipoMiscelaneo.setDescripcion(resultSet.getString(3));
-					listaTipoMiscelaneo.add(tipoMiscelaneo);
-				}//FIN WHILE
-			}catch (SQLException ex) {
-				Notificacion.dialogoException(ex);
-			}//FIN TRY/CATCH
-		}//FIN IF/ELSE
-		return listaTipoMiscelaneo;
-	}//FIN METODO	
-	
-	//METODO PARA HACER UPDATE EN LA TABLA TIPOMISCELANEO
-	@Override
-	public boolean modificar(Connection connection, Object TipoMiscelaneo) {
-		String query = "UPDATE tipoMiscelaneo "
-				+ "SET  codigo = ?, descripcion = ? "
-				+ "WHERE sysPK = ?";
-		try {
-			TipoMiscelaneo tipoMiscelaneo = (TipoMiscelaneo)TipoMiscelaneo;
-			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
-			preparedStatement.setString(1, tipoMiscelaneo.getCodigo());
-			preparedStatement.setString(2, tipoMiscelaneo.getDescripcion());
-			preparedStatement.setInt(3, tipoMiscelaneo.getSysPk());
-			preparedStatement.execute();
-			return true;
-		} catch (SQLException e) {
-			Notificacion.dialogoException(e);
 			return false;
 		}//FIN TRY/CATCH
-	}//FIN METODO	
+	}//FIN METODO
 	
-	//METODO PARA HACER DELETE EN LA TABLA TIPOMISCELANEO
-	@Override
-	public boolean eliminar(Connection connection, Object TipoMiscelaneo) {
-		String query = "DELETE FROM tipoMiscelaneo WHERE sysPK = ?";
-		try {	
-			TipoMiscelaneo tipoMiscelaneo = (TipoMiscelaneo)TipoMiscelaneo;
-			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
-			preparedStatement.setInt(1, tipoMiscelaneo.getSysPk());
-			preparedStatement.execute();
-			return true;
-		} catch (SQLException e) {
-			Notificacion.dialogoException(e);
-			return false;
-		}//FIN TRY/CATCH	
-	}//FIN METODO		
-	
-	//METODO PARA OBTENER EL ULTIMO SYSPK AGREGADO A LA TABLA 
-	public int ultimoSysPk(Connection connection) {
-		String query = "SELECT sysPK FROM tipoMiscelaneo order by sysPK asc";
-		int ultimoSysPk = 0;
+	//METODO PARA OBTENER UN REGISTRO
+	public static ArrayList<TipoMiscelaneo> readTipoMiscelaneo(Connection connection) {
+		ArrayList<TipoMiscelaneo> arrayListTipoMiscelaneo = new ArrayList<TipoMiscelaneo>();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion, Status FROM tipoMiscelaneo";
 		try {
-			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery(query);
-			while (resultSet.next())
-				ultimoSysPk = resultSet.getInt(1);
-			return ultimoSysPk;
-		}catch (SQLException e) {
-			Notificacion.dialogoException(e);
+			Statement sentencia = connection.createStatement();
+			ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				TipoMiscelaneo tipoMiscelaneo = new TipoMiscelaneo();
+				tipoMiscelaneo.setSysPK(resultados.getInt(1));
+				tipoMiscelaneo.setCodigo(resultados.getString(2));
+				tipoMiscelaneo.setDescripcion(resultados.getString(3));
+				tipoMiscelaneo.setStatus(resultados.getInt(4));
+				arrayListTipoMiscelaneo.add(tipoMiscelaneo);
+			}//FIN WHILE
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
 		}//FIN TRY/CATCH
-		return ultimoSysPk;
+		return arrayListTipoMiscelaneo;
+	}//FIN METODO
+	
+	//METODO PARA OBTENER UN REGISTRO
+	public static TipoMiscelaneo readTipoMiscelaneo(Connection connection, int sysPK) {
+		TipoMiscelaneo tipoMiscelaneo = new TipoMiscelaneo();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion, Status from tipoMiscelaneo WHERE Sys_PK = " + sysPK;
+		try {
+			Statement sentencia = connection.createStatement();
+			ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				tipoMiscelaneo.setSysPK(resultados.getInt(1));
+				tipoMiscelaneo.setCodigo(resultados.getString(2));
+				tipoMiscelaneo.setDescripcion(resultados.getString(3));
+				tipoMiscelaneo.setStatus(resultados.getInt(4));
+			}//FIN WHILE
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+		}//FIN TRY/CATCH
+		return tipoMiscelaneo;
+	}//FIN METODO
+	
+	//METODO PARA OBTENER UN REGISTRO
+	public static ArrayList<TipoMiscelaneo> readTipoMiscelaneo(Connection connection, String like) {
+		ArrayList<TipoMiscelaneo> arrayListTipoMiscelaneo = new ArrayList<TipoMiscelaneo>();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion, Status FROM tipoMiscelaneo WHERE Codigo LIKE '%" + like + "%' OR Descripcion LIKE '%" + like + "%';";
+		try {
+			Statement sentencia = connection.createStatement();
+			ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				TipoMiscelaneo tipoMiscelaneo = new TipoMiscelaneo();
+				tipoMiscelaneo.setSysPK(resultados.getInt(1));
+				tipoMiscelaneo.setCodigo(resultados.getString(2));
+				tipoMiscelaneo.setDescripcion(resultados.getString(3));
+				tipoMiscelaneo.setStatus(resultados.getInt(4));
+				arrayListTipoMiscelaneo.add(tipoMiscelaneo);
+			}//FIN WHILE
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+		}//FIN TRY/CATCH
+		return arrayListTipoMiscelaneo;
+	}//FIN METODO
+	
+	//METODO PARA CREAR UN REGISTRO
+	public static boolean updateTipoMiscelaneo(Connection connection, TipoMiscelaneo tipoMiscelaneo) {
+		String consulta = "UPDATE tipoMiscelaneo SET Codigo = ?, Descripcion = ?, Status = ? WHERE Sys_PK = ?";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setString(1, tipoMiscelaneo.getCodigo());
+			sentenciaPreparada.setString(2, tipoMiscelaneo.getDescripcion());
+			sentenciaPreparada.setInt(3,  tipoMiscelaneo.getStatusFK());
+			sentenciaPreparada.setInt(4, tipoMiscelaneo.getSysPK());
+			sentenciaPreparada.execute();
+			return true;
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+			return false;
+		}//FIN TRY/CATCH 	
+	}//FIN METODO
+	
+	//METODO PARA CREAR UN REGISTRO
+	public static boolean deleteTipoMiscelaneo(Connection connection, TipoMiscelaneo tipoMiscelaneo) {
+		String consulta = "DELETE FROM tipoMiscelaneo WHERE Sys_PK = ?";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setInt(1, tipoMiscelaneo.getSysPK());
+			sentenciaPreparada.execute();
+			return true;
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+			return false;
+		}//FIN TRY/CATCH
+	}//FIN METODO
+	
+	//METODO PARA CONVERTIR ARRAYLIST EN OBSERVABLELIST
+	public static ObservableList<TipoMiscelaneo> toObservableList(ArrayList<TipoMiscelaneo> arrayList) {
+		ObservableList<TipoMiscelaneo> listaObservableTipoMiscelaneo = FXCollections.observableArrayList();
+		for (TipoMiscelaneo tipoMiscelaneo : arrayList) 
+			listaObservableTipoMiscelaneo.add(tipoMiscelaneo);
+		return listaObservableTipoMiscelaneo;
 	}//FIN METODO
 
 }//FIN CLASE
---
+++ src/mx/shf6/produccion/view/DialogoTipoMiscelaneo.java
@@ -0,0 +1,118 @@
+package mx.shf6.produccion.view;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
+import mx.shf6.produccion.MainApp;
+import mx.shf6.produccion.model.Status;
+import mx.shf6.produccion.model.TipoMiscelaneo;
+import mx.shf6.produccion.model.dao.TipoMiscelaneoDAO;
+import mx.shf6.produccion.utilities.Notificacion;
+
+public class DialogoTipoMiscelaneo {
+
+	//PROPIEDADES
+	private MainApp mainApp;
+	private TipoMiscelaneo tipoMiscelaneo;
+	
+	//VARIABLES
+	private int opcion;
+	
+	//CONSTANTES
+	public static final int CREAR = 1;
+	public static final int VER = 2;
+	public static final int EDITAR = 3;
+	
+	//COMPONENTES INTERFAZ
+	@FXML private TextField campoTextoCodigo;
+	@FXML private TextField campoTextoDescripcion;
+	@FXML private ComboBox<String> comboBoxStatus;
+	
+	//INICIA COMPONENTES INTERFAZ USUARIO
+	@FXML private void initialize() {
+		this.tipoMiscelaneo = new TipoMiscelaneo();
+		ObservableList<String> listaStatus = FXCollections.observableArrayList("No Visible", "Visible");
+		this.comboBoxStatus.setItems(listaStatus);
+	}//FIN METODO
+	
+	//ACCESO CLASE PRINCIPAL
+	public void setMainApp(MainApp mainApp, TipoMiscelaneo tipoMiscelaneo, int opcion) {
+		this.mainApp = mainApp;
+		this.tipoMiscelaneo = tipoMiscelaneo;
+		this.opcion = opcion;
+		this.inicializarComponentes();
+	}//FIN METODO
+	
+	//INICIALIZA COMPONENTE
+	private void inicializarComponentes() {		
+		if (this.opcion == CREAR) {
+			this.campoTextoCodigo.setText("");
+			this.campoTextoCodigo.setDisable(false);
+			this.campoTextoDescripcion.setText("");
+			this.campoTextoDescripcion.setDisable(false);
+			this.comboBoxStatus.getSelectionModel().select("");
+			this.comboBoxStatus.setDisable(false);
+		} else if (this.opcion == VER) {
+			this.campoTextoCodigo.setText(this.tipoMiscelaneo.getCodigo());
+			this.campoTextoCodigo.setDisable(true);
+			this.campoTextoDescripcion.setText(this.tipoMiscelaneo.getDescripcion());
+			this.campoTextoDescripcion.setDisable(true);
+			this.comboBoxStatus.getSelectionModel().select(this.tipoMiscelaneo.getStatus());
+			this.comboBoxStatus.setDisable(true);
+		} else if (this.opcion == EDITAR) {
+			this.campoTextoCodigo.setText(this.tipoMiscelaneo.getCodigo());
+			this.campoTextoCodigo.setDisable(false);
+			this.campoTextoDescripcion.setText(this.tipoMiscelaneo.getDescripcion());
+			this.campoTextoDescripcion.setDisable(false);
+			this.comboBoxStatus.getSelectionModel().select(this.tipoMiscelaneo.getStatus());
+			this.comboBoxStatus.setDisable(false);
+		}//FIN METODO
+	}//FIN METODO
+	
+	//VALIDAR DATOS
+	private boolean validarDatos() {
+		if (this.campoTextoCodigo.getText().isEmpty()) {
+			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Código\" no puede estar vacio");
+			return false;
+		} else if (this.campoTextoDescripcion.getText().isEmpty()) {
+			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Descripción\" no puede estar vacio");
+			return false;
+		} else if (this.comboBoxStatus.getSelectionModel().getSelectedItem().isEmpty()) {
+			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Descripción\" no puede estar vacio");
+			return false;
+		}//FIN IF/ESLE
+		return true;
+	}//FIN METODO
+	
+	//MANEJADORES COMPONENTES	
+	@FXML private void vmanejadorBotonAceptar() {
+		if (this.validarDatos() && this.opcion == CREAR) {
+			this.tipoMiscelaneo.setCodigo(this.campoTextoCodigo.getText());
+			this.tipoMiscelaneo.setDescripcion(this.campoTextoDescripcion.getText());
+			this.tipoMiscelaneo.setStatus(Status.toInt(this.comboBoxStatus.getSelectionModel().getSelectedItem()));
+			if (TipoMiscelaneoDAO.createTipoMiscelaneo(this.mainApp.getConnection(), this.tipoMiscelaneo)) {
+				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se creo de forma correcta");
+				this.mainApp.getEscenarioDialogos().close();
+			} else
+				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "No se pudo crear el registro, revisa que la información sea correcta");
+		} else if (this.validarDatos() && this.opcion == EDITAR) {
+			this.tipoMiscelaneo.setCodigo(this.campoTextoCodigo.getText());
+			this.tipoMiscelaneo.setDescripcion(this.campoTextoDescripcion.getText());
+			this.tipoMiscelaneo.setStatus(Status.toInt(this.comboBoxStatus.getSelectionModel().getSelectedItem()));
+			if (TipoMiscelaneoDAO.updateTipoMiscelaneo(this.mainApp.getConnection(), this.tipoMiscelaneo)) {
+				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se actualizo de forma correcta");
+				this.mainApp.getEscenarioDialogos().close();
+			} else
+				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "No se pudo actualizar el registro, revisa que la información sea correcta");
+		} else if (this.validarDatos() && this.opcion == VER)
+			this.mainApp.getEscenarioDialogos().close();			
+	}//FIN METODO
+	
+	@FXML private void manejadorBotonCerrar() {
+		this.mainApp.getEscenarioDialogos().close();
+	}//FIN METODO
+	
+}//FIN CLASE
