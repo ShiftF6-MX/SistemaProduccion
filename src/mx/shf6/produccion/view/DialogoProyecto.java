497adc5dee5263198945f60ea326780ee7eca9a5
Emmanuel Ostria <emmanuel.ostria@gruposhift-f6.com.mx>
2019-04-02 14:46:26 -0600

    Se agrego PantallaProyecto y DialogoProyecto

---
+++ src/mx/shf6/produccion/view/DialogoProyecto.java
@@ -0,0 +1,13 @@
+package mx.shf6.produccion.view;
+
+import mx.shf6.produccion.MainApp;
+import mx.shf6.produccion.model.Proyecto;
+
+public class DialogoProyecto {
+
+	public void setMainApp(MainApp mainApp, Proyecto proyecto, int opcion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
