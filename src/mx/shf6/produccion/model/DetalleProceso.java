17434f3dea59301e71e6e5d8f7012abb0766faa5
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-05-29 13:21:22 -0500

    Clases creadas DetalleProceso y Proceso

---
+++ src/mx/shf6/produccion/model/DetalleProceso.java
@@ -0,0 +1,152 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class DetalleProceso {
+
+	//PROPIEDADES
+	private IntegerProperty sysPK;
+	private IntegerProperty operacion;
+	private StringProperty descripcion;
+	private IntegerProperty tiempoPreparacion;
+	private IntegerProperty tiempoOperacion;
+	private IntegerProperty centroTrabajoFK;
+	private IntegerProperty grupoTrabajoFK;
+	private IntegerProperty procesoFK;
+
+	//VARIABLES
+
+	//CONSTANTES
+
+	//CONSTRUCTOR VACIO
+	public DetalleProceso() {
+		this(0,0,"",0,0,0,0,0);
+	}//FIN CONSTRUCTOR
+
+	//CONSTRUCTOR CON PARAMETROS
+	public DetalleProceso(Integer sysPK, Integer operacion, String descripcion, Integer tiempoPreparacion, Integer tiempoOperacion, Integer centroTabajo, Integer grupoTrabajo, Integer procesoFK) {
+		this.sysPK = new SimpleIntegerProperty(sysPK);
+		this.operacion = new SimpleIntegerProperty(operacion);
+		this.descripcion = new SimpleStringProperty(descripcion);
+		this.tiempoPreparacion = new SimpleIntegerProperty(tiempoPreparacion);
+		this.tiempoOperacion = new SimpleIntegerProperty(tiempoOperacion);
+		this.centroTrabajoFK = new SimpleIntegerProperty(grupoTrabajo);
+		this.procesoFK = new SimpleIntegerProperty(procesoFK);
+	}//FIN CONSTRUCTOR
+
+	//METODOS PARA ACCESO A "SYSPK"
+	public Integer getSysPK() {
+		return sysPK.get();
+	}//FIN METODO
+
+	public void setSysPK(Integer sysPK) {
+		this.sysPK.set(sysPK);
+	}//FIN METODO
+
+	public IntegerProperty sysPKProperty() {
+    	return sysPK;
+    }//FIN METODO
+	//FIN METODOS "SYSPK"
+
+	//METODOS PARA ACCESO A "OPERACION"
+	public Integer getOperacion() {
+		return operacion.get();
+	}//FIN METODO
+
+	public void setOperacion(Integer operacion) {
+		this.operacion.set(operacion);
+	}//FIN METODO
+
+	public IntegerProperty operacionProperty() {
+    	return operacion;
+    }//FIN METODO
+	//FIN METODOS "OPERACION"
+
+	//METODOS PARA ACCESO A "DESCRIPCION"
+	public String getDescripcion() {
+		return descripcion.get();
+	}//FIN METODO
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion.set(descripcion);
+	}//FIN METODO
+
+	public StringProperty descripcionProperty() {
+    	return descripcion;
+    }//FIN METODO
+	//FIN METODOS "DESCRIPCION"
+
+	//METODOS PARA ACCESO A "PREPARACION"
+	public Integer getTiempoPreparacion() {
+		return tiempoPreparacion.get();
+	}//FIN METODO
+
+	public void setTiempoPreparacion(Integer tiempoPreparacion) {
+		this.tiempoPreparacion.set(tiempoPreparacion);
+	}//FIN METODO
+
+	public IntegerProperty tiempoPreparacionProperty() {
+    	return tiempoPreparacion;
+    }//FIN METODO
+	//FIN METODOS "PREPARACION"
+
+	//METODOS PARA ACCESO A "TIEMPOOPERACION"
+	public Integer getTiempoOperacion() {
+		return tiempoOperacion.get();
+	}//FIN METODO
+
+	public void setTiempoOperacion(Integer tiempoOperacion) {
+		this.tiempoOperacion.set(tiempoOperacion);
+	}//FIN METODO
+
+	public IntegerProperty tiempoOperacionProperty() {
+    	return tiempoOperacion;
+    }//FIN METODO
+	//FIN METODOS "TIEMPOOPERACION"
+
+	//METODOS PARA ACCESO A "CENTROTRABAJOFK"
+	public Integer getCentroTrabajoFK() {
+		return centroTrabajoFK.get();
+	}//FIN METODO
+
+	public void setCentroTrabajoFK(Integer centroTrabajoFK) {
+		this.centroTrabajoFK.set(centroTrabajoFK);
+	}//FIN METODO
+
+	public IntegerProperty centroTrabajoFKProperty() {
+    	return centroTrabajoFK;
+    }//FIN METODO
+	//FIN METODOS "CENTROTRABAJOFK"
+
+	//METODOS PARA ACCESO A "GRUPOTRABAJOFK"
+	public Integer getGrupoTrabajoFK() {
+		return grupoTrabajoFK.get();
+	}//FIN METODO
+
+	public void setGrupoTrabajoFK(Integer grupoTrabajoFK) {
+		this.grupoTrabajoFK.set(grupoTrabajoFK);
+	}//FIN METODO
+
+	public IntegerProperty grupoTrabajoFKProperty() {
+	    return grupoTrabajoFK;
+	}//FIN METODO
+	//FIN METODOS "GRUPOTRABAJOFK"
+
+	//METODOS PARA ACCESO A "PROCESOFK"
+	public Integer getProcesoFK() {
+		return centroTrabajoFK.get();
+	}//FIN METODO
+
+	public void setProcesoFK(Integer procesoFK) {
+		this.procesoFK.set(procesoFK);
+	}//FIN METODO
+
+	public IntegerProperty procesoFKProperty() {
+    	return procesoFK;
+    }//FIN METODO
+	//FIN METODOS "PROCESOFK"
+
+}//FIN CLASE
---
+++ src/mx/shf6/produccion/model/Proceso.java
@@ -0,0 +1,169 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Proceso {
+
+	//PROPIEDADES
+	private IntegerProperty sysPK;
+	private IntegerProperty fecha;
+	private IntegerProperty cantidad;
+	private IntegerProperty ordenamiento;
+	private IntegerProperty nivel;
+	private StringProperty destino;
+	private IntegerProperty tiempo;
+	private IntegerProperty componenteFK;
+	private IntegerProperty empleadoFK;
+
+	//VARIABLES
+
+	//CONSTANTES
+
+	//CONSTRUCTOR VACIO
+	public Proceso() {
+		this(0,0,0,0,0,"",0,0,0);
+	}//FIN CONSTRUCTOR
+
+	//CONSTRUCTOR CON PARAMETROS
+	public Proceso(Integer sysPK, Integer fecha, Integer cantidad, Integer ordenamiento, Integer nivel, String destino, Integer tiempo, Integer componenteFK, Integer empleadoFK){
+		this.sysPK = new SimpleIntegerProperty(sysPK);
+		this.fecha = new SimpleIntegerProperty(fecha);
+		this.cantidad = new SimpleIntegerProperty(cantidad);
+		this.ordenamiento = new SimpleIntegerProperty(ordenamiento);
+		this.nivel = new SimpleIntegerProperty(nivel);
+		this.destino = new SimpleStringProperty(destino);
+		this.tiempo = new SimpleIntegerProperty(tiempo);
+		this.componenteFK = new SimpleIntegerProperty(componenteFK);
+		this.empleadoFK = new SimpleIntegerProperty(empleadoFK);
+	}//FIN CONSTRUCTOR
+
+	//METODOS PARA ACCESO A "SYSPK"
+	public Integer getSysPK() {
+		return sysPK.get();
+	}//FIN METODO
+
+	public void setSysPK(Integer sysPK) {
+		this.sysPK.set(sysPK);
+	}//FIN METODO
+
+	public IntegerProperty sysPKProperty() {
+    	return sysPK;
+    }//FIN METODO
+	//FIN METODOS "SYSPK"
+
+	//METODOS PARA ACCESO A FECHA
+	public Integer getFecha() {
+		return fecha.get();
+	}//FIN METODO
+
+	public void setFecha(Integer fecha) {
+		this.fecha.set(fecha);
+	}//FIN METODO
+
+	public IntegerProperty fechaProperty() {
+		return fecha;
+	}//FIN METODO
+	//FIN METODOS "FECHA"
+
+	//METODOS PARA ACCESO A "CANTIDAD"
+	public Integer getCantidad() {
+        return cantidad.get();
+    }//FIN METODO
+
+	public void setCantidad(Integer cantidad) {
+        this.cantidad.set(cantidad);
+    }//FIN METODO
+
+	public IntegerProperty cantidadProperty() {
+        return cantidad;
+    }//FIN METODO
+	//FIN METODOS "CANTIDAD"
+
+	//METODO PARA ACCESO A "ORDENAMIENTO"
+	public Integer getOrdenamiento() {
+        return ordenamiento.get();
+    }//FIN METODO
+
+	 public void setOrdenamiento(Integer ordenamiento) {
+		 this.ordenamiento.set(ordenamiento);
+	 }//FIN METODO
+
+	 public IntegerProperty ordenamientoProperty() {
+	      return ordenamiento;
+	 }//FIN METODO
+	//FIN METODOS "ORDENAMIENTO"
+
+	 //METODO PARA ACCESO A "NIVEL"
+	public Integer getNivel() {
+		return nivel.get();
+	}//FIN METODO
+
+	public void setNivel(Integer nivel) {
+    	this.nivel.set(nivel);
+    }//FIN METODO
+
+	public IntegerProperty nivelProperty() {
+    	return nivel;
+    }//FIN METODO
+	//FIN METODOS "NIVEL"
+
+	//METODOS PARA ACCESO A "DESTINO"
+	public String getDestino() {
+		return destino.get();
+	}//FIN METODO
+
+	public void setDestino(String destino) {
+    	this.destino.set(destino);
+    }//FIN METODO
+
+	public StringProperty destinoProperty() {
+    	return destino;
+    }//FIN METODO
+	//FIN METODOS "DESTINO"
+
+	//METODOS PARA ACCESO A "TIEMPO"
+	public Integer getTiempo() {
+		return tiempo.get();
+	}//FIN METODO
+
+	 public void setTiempo(Integer tiempo) {
+	    this.tiempo.set(tiempo);
+	 }//FIN METODO
+
+	 public IntegerProperty tiempoProperty() {
+	   	return tiempo;
+	 }//FIN METODO
+	//FIN METODOS A "TIEMPO"
+
+	//METODOS PARA ACCESO A "COMPONENTEFK"
+	public Integer getComponenteFK() {
+		return componenteFK.get();
+	}//FIN METODO
+
+	public void setComponenteFK(Integer componenteFK) {
+    	this.componenteFK.set(componenteFK);
+    }//FIN METODO
+
+	public IntegerProperty componenteFKProperty() {
+    	return componenteFK;
+    }//FIN METODO
+	//FIN METODOS "COMPONENTEFK"
+
+	//METODOS PARA ACCESO A "EMPLEADOFK"
+	public Integer getEmpleadoFK() {
+		return empleadoFK.get();
+	}//FIN METODO
+
+	 public void setEmpleadoFK(Integer empleadoFK) {
+	    this.empleadoFK.set(empleadoFK);
+	 }//FIN METODO
+
+	 public IntegerProperty empleadoFK() {
+	    return empleadoFK;
+	 }//FIN METODO
+	//FIN METODOS "EMPLEADOFK"
+
+}//FIN CLASE
