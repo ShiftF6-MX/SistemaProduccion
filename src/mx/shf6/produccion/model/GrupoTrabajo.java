c140e15cc9c102d0910ed965eb060d1bae11c39b
Rago1995 <raul.gomez@gruposhift-f6.com.mx>
2019-05-30 13:26:06 -0500

    Clases GrupoTrabajo
    Se creo la clase Modelo y DAO GrupoTrabajo

---
+++ src/mx/shf6/produccion/model/GrupoTrabajo.java
@@ -0,0 +1,67 @@
+package mx.shf6.produccion.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+	public class GrupoTrabajo {
+		
+		//PROPIEDADES
+		private IntegerProperty sysPK;
+		private StringProperty codigo;
+		private StringProperty descripcion;
+		
+		//VARIABLES
+		
+		//CONTRUCTORES
+		public GrupoTrabajo() {
+			this(0,"","");
+
+		}//FIN CONSTRUCTOR
+		public GrupoTrabajo(Integer sysPK, String codigo, String descripcion) {
+			this.sysPK = new SimpleIntegerProperty(sysPK);
+			this.codigo = new SimpleStringProperty(codigo);
+			this.descripcion = new SimpleStringProperty(descripcion);
+		}//FIN CONSTRUCTOR
+		
+		
+		//METODOS
+		public int getSysPK() {
+			return sysPK.get();
+		}//FIN METODO
+		
+		public void setSysPK(Integer sysPK) {
+			this.sysPK.set(sysPK);	
+		}//FIN METODO
+		
+		public IntegerProperty sysPKProperty() {
+			return sysPK;
+		}//FIN METODO
+		
+		public String getCodigo() {
+			return codigo.get();
+		}//FIN METODO
+		
+		public void setCodigo(String codigo) {
+			this.codigo.set(codigo);	
+		}//FIN METODO
+		
+		public StringProperty codigoProperty() {
+			return codigo;
+		}//FIN METODO
+		
+		public String getDescripcion() {
+			return descripcion.get();
+		}//FIN METODO
+
+		public void setDescripcion(String descripcion) {
+			this.descripcion.set(descripcion);	
+		}//FIN METODO
+		
+		public StringProperty descripcionProperty() {
+			return descripcion;
+		}//FIN METODO
+	}//FIN CLASE
+
+
---
+++ src/mx/shf6/produccion/model/dao/GrupoTrabajoDAO.java
@@ -0,0 +1,122 @@
+package mx.shf6.produccion.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import com.sun.org.apache.regexp.internal.REUtil;
+
+import mx.shf6.produccion.model.GrupoTrabajo;
+
+
+
+public class GrupoTrabajoDAO {
+	// METODO PARA CREAR UN REGISTRO
+	public static boolean createGrupoTrabajo(Connection connection, GrupoTrabajo grupoTrabajo) {
+		String consulta = "INSERT INTO grupostrabajo (Codigo, Descripcion) VALUES (?, ?)";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setString(1, grupoTrabajo.getCodigo());
+			sentenciaPreparada.setString(2, grupoTrabajo.getDescripcion());
+			sentenciaPreparada.execute();
+			return true;
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error " + ex);
+			return false;
+		} // FIN TRY/CATCH
+	}// FIN METODO
+
+	// METODO PARA OBTENER UN REGISTRO
+	public static ArrayList<GrupoTrabajo> readGrupoTrabajo(Connection connection) {
+		ArrayList<GrupoTrabajo> arrayListGrupoTrabajo = new ArrayList<GrupoTrabajo>();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion FROM grupostrabajo";
+		try {
+			Statement sentencia = connection.createStatement();
+			java.sql.ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				GrupoTrabajo grupotrabajo = new GrupoTrabajo();
+				grupotrabajo.setSysPK(resultados.getInt(1));
+				grupotrabajo.setCodigo(resultados.getString(2));
+				grupotrabajo.setDescripcion(resultados.getString(3));
+				arrayListGrupoTrabajo.add(grupotrabajo);
+			} // FIN WHILE
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error " + ex);
+		} // FIN TRY/CATCH
+		return arrayListGrupoTrabajo;
+	}// FIN METODO
+
+	// METODO PARA OBTENER UN REGISTRO
+	public static GrupoTrabajo readGrupoTrabajo(Connection connection, int sysPK) {
+		GrupoTrabajo grupotrabajo = new GrupoTrabajo();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion from grupostrabajo WHERE Sys_PK = " + sysPK;
+		try {
+			Statement sentencia = connection.createStatement();
+			java.sql.ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				grupotrabajo.setSysPK(resultados.getInt(1));
+				grupotrabajo.setCodigo(resultados.getString(2));
+				grupotrabajo.setDescripcion(resultados.getString(3));
+
+			} // FIN WHILE
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error " + ex);
+		} // FIN TRY/CATCH
+		return grupotrabajo;
+	}// FIN METODO
+
+	// METODO PARA OBTENER UN REGISTRO
+	public static ArrayList<GrupoTrabajo> readGrupoTrabajo(Connection connection, String like) {
+		ArrayList<GrupoTrabajo> arrayListaGrupoTrabajo = new ArrayList<GrupoTrabajo>();
+		String consulta = "SELECT Sys_PK, Codigo, Descripcion FROM grupostrabajo WHERE Codigo LIKE '%" + like + "%';";
+		try {
+			Statement sentencia = connection.createStatement();
+			java.sql.ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				GrupoTrabajo grupos = new GrupoTrabajo();
+				grupos.setSysPK(resultados.getInt(1));
+				grupos.setCodigo(resultados.getString(2));
+				grupos.setDescripcion(resultados.getString(3));
+				arrayListaGrupoTrabajo.add(grupos);
+			} // FIN WHILE
+		} catch (SQLException ex) {
+			// Notificacion.dialogoException(ex);
+		} // FIN TRY/CATCH
+		return arrayListaGrupoTrabajo;
+	}// FIN METODO
+
+	// METODO PARA EDITAR UN REGISTRO
+	public static boolean updateGrupoTrabajo(Connection connection, GrupoTrabajo grupotrabajo) {
+		String consulta = "UPDATE grupostrabajo SET Codigo = ?, Descripcion = ? WHERE Sys_PK = ?";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setString(1, grupotrabajo.getCodigo());
+			sentenciaPreparada.setString(2, grupotrabajo.getDescripcion());
+			sentenciaPreparada.setInt(3, grupotrabajo.getSysPK());
+			sentenciaPreparada.execute();
+			return true;
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error " + ex);
+			return false;
+		} // FIN TRY/CATCH
+	}// FIN METODO
+
+	// METODO PARA ELIMINAR UN REGISTRO
+	public static boolean deleteGrupoTrabajo(Connection connection, GrupoTrabajo grupotrabajo) {
+		String consulta = "DELETE FROM grupostrabajo WHERE Sys_PK = ?";
+		try {
+			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
+			sentenciaPreparada.setInt(1, grupotrabajo.getSysPK());
+			sentenciaPreparada.execute();
+			return true;
+		} catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Error " + ex);
+			return false;
+		} // FIN TRY/CATCH
+	}// FIN METODO
+
+}// FIN CLASE
