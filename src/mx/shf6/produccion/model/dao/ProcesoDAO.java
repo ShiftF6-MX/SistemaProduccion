30611e8d37d11beca607d2c4179e2582a437af43
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-05-31 17:04:32 -0500

    Modificación en la clase DetalleProceso se cambio el nombre y tipo de dato de 'Destino' por el de 'CentroTrabajoFK'.

--- src/mx/shf6/produccion/model/dao/ProcesoDAO.java
+++ src/mx/shf6/produccion/model/dao/ProcesoDAO.java
@@ -13,14 +13,14 @@ public class ProcesoDAO {
 
 	//METODO PARA CREAR UN REGISTRO
 	public static boolean createProceso(Connection connection, Proceso proceso) {
-		String consulta = "INSERT INTO procesos (Fecha, Cantidad, Ordenamiento, Nivel, Destino, ComponenteFK, EmpleadoFK) VALUES (?,?,?,?,?,?,?)";
+		String consulta = "INSERT INTO procesos (Fecha, Cantidad, Ordenamiento, Nivel, CentroTrabajoFK, ComponenteFK, EmpleadoFK) VALUES (?,?,?,?,?,?,?)";
 		try {
 			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
 			sentenciaPreparada.setDate(1, proceso.getFecha());
 			sentenciaPreparada.setInt(2, proceso.getCantidad());
 			sentenciaPreparada.setInt(3, proceso.getOrdenamiento());
 			sentenciaPreparada.setInt(4, proceso.getNivel());
-			sentenciaPreparada.setString(5, proceso.getDestino());
+			sentenciaPreparada.setInt(5, proceso.getCentroTrabajoFK());
 			sentenciaPreparada.setInt(6, proceso.getComponenteFK());
 			sentenciaPreparada.setInt(7, proceso.getEmpleadoFK());
 			sentenciaPreparada.execute();
@@ -34,7 +34,7 @@ public class ProcesoDAO {
 	//METODO PARA OBTENER UN REGISTRO
 	public static ArrayList<Proceso> readProceso(Connection connection) {
 		ArrayList<Proceso> arrayListProceso = new ArrayList<Proceso>();
-		String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, Destino, ComponenteFK, EmpleadoFK FROM procesos";
+		String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentrTrabajoFK, ComponenteFK, EmpleadoFK FROM procesos";
 		try {
 			Statement sentencia = connection.createStatement();
 			ResultSet resultados = sentencia.executeQuery(consulta);
@@ -45,7 +45,7 @@ public class ProcesoDAO {
 				proceso.setCantidad(resultados.getInt(3));
 				proceso.setOrdenamiento(resultados.getInt(4));
 				proceso.setNivel(resultados.getInt(5));
-				proceso.setDestino(resultados.getString(6));
+				proceso.setCentroTrabajoFK(resultados.getInt(6));
 				proceso.setComponenteFK(resultados.getInt(7));
 				proceso.setEmpleadoFK(resultados.getInt(8));
 			}//FIN WHILE
@@ -58,7 +58,7 @@ public class ProcesoDAO {
 	//METODO PARA OBTENER UN REGISTRO POR SYSPK
 		public static Proceso readProceso(Connection connection, int sysPK) {
 			Proceso proceso = new Proceso();
-			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, Destino, ComponenteFK,EmpleadoFK FROM procesos WHERE Sys_PK = " + sysPK;
+			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentroTrabajoFK, ComponenteFK,EmpleadoFK FROM procesos WHERE Sys_PK = " + sysPK;
 			try {
 				Statement sentencia = connection.createStatement();
 				ResultSet resultados = sentencia.executeQuery(consulta);
@@ -68,7 +68,7 @@ public class ProcesoDAO {
 					proceso.setCantidad(resultados.getInt(3));
 					proceso.setOrdenamiento(resultados.getInt(4));
 					proceso.setNivel(resultados.getInt(5));
-					proceso.setDestino(resultados.getString(6));
+					proceso.setCentroTrabajoFK(resultados.getInt(6));
 					proceso.setComponenteFK(resultados.getInt(7));
 					proceso.setEmpleadoFK(resultados.getInt(8));
 				}//FIN WHILE
@@ -81,7 +81,7 @@ public class ProcesoDAO {
 		//METODO PARA OBTENER UN REGISTRO POR LIKE
 		public static ArrayList<Proceso> readProceso(Connection connection, String like) {
 			ArrayList<Proceso> arrayListProceso = new ArrayList<Proceso>();
-			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, Destino, ComponenteFK, EmpleadoFK FROM procesos WHERE Fecha LIKE '%" + like + "%' OR INNER JOIN   LIKE '%" + like + "%';";
+			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentroTrabajoFK, ComponenteFK, EmpleadoFK FROM procesos WHERE Fecha LIKE '%" + like + "%' OR INNER JOIN   LIKE '%" + like + "%';";
 			try {
 				Statement sentencia = connection.createStatement();
 				ResultSet resultados = sentencia.executeQuery(consulta);
@@ -92,7 +92,7 @@ public class ProcesoDAO {
 					proceso.setCantidad(resultados.getInt(3));
 					proceso.setOrdenamiento(resultados.getInt(4));
 					proceso.setNivel(resultados.getInt(5));
-					proceso.setDestino(resultados.getString(6));
+					proceso.setCentroTrabajoFK(resultados.getInt(6));
 					proceso.setComponenteFK(resultados.getInt(7));
 					proceso.setEmpleadoFK(resultados.getInt(8));
 				}//FIN WHILE
@@ -104,14 +104,14 @@ public class ProcesoDAO {
 
 		//METODO PARA ACTUALIZAR UN REGISTRO
 		public static boolean updateProceso(Connection connection, Proceso proceso) {
-			String consulta = "UPDATE procesos SET Fecha = ?, Cantidad = ?, Ordenamiento = ?, Nivel = ?, Destino = ?, ComponenteFK = ?, EmpleadoFK = ? WHERE Sys_PK = ?";
+			String consulta = "UPDATE procesos SET Fecha = ?, Cantidad = ?, Ordenamiento = ?, Nivel = ?, CentroTrabajoFK = ?, ComponenteFK = ?, EmpleadoFK = ? WHERE Sys_PK = ?";
 			try {
 				PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
 				sentenciaPreparada.setDate(1, proceso.getFecha());
 				sentenciaPreparada.setInt(2, proceso.getCantidad());
 				sentenciaPreparada.setInt(3, proceso.getOrdenamiento());
 				sentenciaPreparada.setInt(4, proceso.getNivel());
-				sentenciaPreparada.setString(5, proceso.getDestino());
+				sentenciaPreparada.setInt(5, proceso.getCentroTrabajoFK());
 				sentenciaPreparada.setInt(6, proceso.getComponenteFK());
 				sentenciaPreparada.setInt(7, proceso.getEmpleadoFK());
 				sentenciaPreparada.setInt(8, proceso.getSysPK());
