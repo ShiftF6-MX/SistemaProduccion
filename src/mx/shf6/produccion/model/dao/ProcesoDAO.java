b92f9e648791d46580c76fc18502418e749c219e
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-06-03 13:08:40 -0500

    Modificación a la clase ProcesoDAO Se agregaron métodos para trabajar con objetos y strings

--- src/mx/shf6/produccion/model/dao/ProcesoDAO.java
+++ src/mx/shf6/produccion/model/dao/ProcesoDAO.java
@@ -5,7 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import mx.shf6.produccion.model.Proceso;
 import mx.shf6.produccion.utilities.Notificacion;
 
@@ -23,6 +27,7 @@ public class ProcesoDAO {
 			sentenciaPreparada.setInt(5, proceso.getCentroTrabajoFK());
 			sentenciaPreparada.setInt(6, proceso.getComponenteFK());
 			sentenciaPreparada.setInt(7, proceso.getEmpleadoFK());
+
 			sentenciaPreparada.execute();
 			return true;
 		} catch(SQLException ex) {
@@ -34,11 +39,11 @@ public class ProcesoDAO {
 	//METODO PARA OBTENER UN REGISTRO
 	public static ArrayList<Proceso> readProceso(Connection connection) {
 		ArrayList<Proceso> arrayListProceso = new ArrayList<Proceso>();
-		String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentrTrabajoFK, ComponenteFK, EmpleadoFK FROM procesos";
+		String consulta = "SELECT procesos.Sys_PK, procesos.Fecha, procesos.Cantidad, procesos.Ordenamiento, procesos.Nivel, procesos.CentroTrabajoFK, centrostrabajo.Descripcion, procesos.ComponenteFK, componentes.NumeroParte, procesos.EmpleadoFK, empleados.Nombre FROM procesos INNER JOIN componentes ON procesos.ComponenteFK = componentes.Sys_PK INNER JOIN empleados ON procesos.EmpleadoFK = empleados.Sys_PK INNER JOIN centrostrabajo ON procesos.CentroTrabajoFK = centrostrabajo.Sys_PK";
 		try {
 			Statement sentencia = connection.createStatement();
 			ResultSet resultados = sentencia.executeQuery(consulta);
-			while(resultados.next()) {
+			while (resultados.next()) {
 				Proceso proceso = new Proceso();
 				proceso.setSysPK(resultados.getInt(1));
 				proceso.setFecha(resultados.getDate(2));
@@ -46,8 +51,11 @@ public class ProcesoDAO {
 				proceso.setOrdenamiento(resultados.getInt(4));
 				proceso.setNivel(resultados.getInt(5));
 				proceso.setCentroTrabajoFK(resultados.getInt(6));
-				proceso.setComponenteFK(resultados.getInt(7));
-				proceso.setEmpleadoFK(resultados.getInt(8));
+				proceso.setNombreCentroTrabajo(resultados.getString(7));
+				proceso.setComponenteFK(resultados.getInt(8));
+				proceso.setNombreComponente(resultados.getString(9));
+				proceso.setEmpleadoFK(resultados.getInt(10));
+				proceso.setNombreEmpleado(resultados.getString(11));
 			}//FIN WHILE
 		} catch(SQLException ex) {
 			Notificacion.dialogoException(ex);
@@ -62,7 +70,7 @@ public class ProcesoDAO {
 			try {
 				Statement sentencia = connection.createStatement();
 				ResultSet resultados = sentencia.executeQuery(consulta);
-				while(resultados.next()) {
+				while (resultados.next()) {
 					proceso.setSysPK(resultados.getInt(1));
 					proceso.setFecha(resultados.getDate(2));
 					proceso.setCantidad(resultados.getInt(3));
@@ -81,11 +89,11 @@ public class ProcesoDAO {
 		//METODO PARA OBTENER UN REGISTRO POR LIKE
 		public static ArrayList<Proceso> readProceso(Connection connection, String like) {
 			ArrayList<Proceso> arrayListProceso = new ArrayList<Proceso>();
-			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentroTrabajoFK, ComponenteFK, EmpleadoFK FROM procesos WHERE Fecha LIKE '%" + like + "%' OR INNER JOIN   LIKE '%" + like + "%';";
+			String consulta = "SELECT Sys_PK, Fecha, Cantidad, Ordenamiento, Nivel, CentroTrabajoFK, ComponenteFK, EmpleadoFK FROM procesos WHERE componentes.NumeroParte LIKE " + like + "%'";
 			try {
 				Statement sentencia = connection.createStatement();
 				ResultSet resultados = sentencia.executeQuery(consulta);
-				while(resultados.next()) {
+				while (resultados.next()) {
 					Proceso proceso = new Proceso();
 					proceso.setSysPK(resultados.getInt(1));
 					proceso.setFecha(resultados.getDate(2));
@@ -113,8 +121,9 @@ public class ProcesoDAO {
 				sentenciaPreparada.setInt(4, proceso.getNivel());
 				sentenciaPreparada.setInt(5, proceso.getCentroTrabajoFK());
 				sentenciaPreparada.setInt(6, proceso.getComponenteFK());
-				sentenciaPreparada.setInt(7, proceso.getEmpleadoFK());
+				sentenciaPreparada.setInt(7, proceso.getEmpleadoFK());	
 				sentenciaPreparada.setInt(8, proceso.getSysPK());
+				
 				sentenciaPreparada.execute();
 				return true;
 			} catch (SQLException ex) {
@@ -137,6 +146,13 @@ public class ProcesoDAO {
 			}//FIN TRY/CATCH
 		}//FIN METODO
 
-
+		//METODO PARA CONVERTIR ARRAYLIST EN OBSERVABLELIST
+		public static ObservableList<Proceso> toObservableList(ArrayList<Proceso> arrayList) {
+			ObservableList<Proceso> listaObservableProceso = FXCollections.observableArrayList();
+			for(Proceso proceso : arrayList) {
+				listaObservableProceso.add(proceso);
+			}
+		    return listaObservableProceso;
+		}//FIN METODO
 
 }//FIN CLASE
