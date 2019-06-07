11e2e6c7267c8276cb061be28c8b915f47bfd7c7
Rago1995 <raul.gomez@gruposhift-f6.com.mx>
2019-06-07 09:35:17 -0500

    Interfaz Usuario Puesto
    Se modifico los margenes y diseño de la interfaz

--- src/mx/shf6/produccion/view/DialogoPuesto.java
+++ src/mx/shf6/produccion/view/DialogoPuesto.java
@@ -10,6 +10,7 @@ import mx.shf6.produccion.MainApp;
 import mx.shf6.produccion.model.Puesto;
 import mx.shf6.produccion.model.dao.PuestoDAO;
 import mx.shf6.produccion.utilities.Notificacion;
+import mx.shf6.produccion.utilities.RestriccionTextField;
 
 public class DialogoPuesto {
 
@@ -19,32 +20,29 @@ public class DialogoPuesto {
 	private Connection conexion;
 	
 	//VARIABLES
-	
 	private int opcion;
 	
 	//CONSTANTES
-	
 	static final int CREAR = 1;
 	static final int VER = 2;
 	static final int EDITAR = 3;
 	
 	//COMPONENTES INTERFAZ
-	
-	@FXML
-	private TextField campoTextoCodigo;
-	@FXML
-	private TextField campoTextoDescripcion;
+	@FXML private TextField campoTextoCodigo;
+	@FXML private TextField campoTextoDescripcion;
 	
 	//INICIA COMPONENTES INTERFAZ DE USUARIO
-	@FXML
-	private void initialize(){
-		//this.puesto = new Puesto();		
+	@FXML private void initialize(){
+		RestriccionTextField.limitarNumeroCaracteres(campoTextoCodigo, 8);
+		
 	}
+	
 	//ACCESO A LA CLASE PRINCIPAL
 	public void setMainApp(MainApp mainApp, Puesto puesto, int opcion) {
 		this.mainApp = mainApp;
 		this.puesto = puesto;
 		this.opcion = opcion;
+		this.conexion = this.mainApp.getConnection();
 		this.inicializarComponentes();  
 		
 	}
@@ -64,7 +62,7 @@ public class DialogoPuesto {
 		}else if (opcion == EDITAR) {
 			this.campoTextoCodigo.setText(puesto.getCodigo());
 			this.campoTextoCodigo.setDisable(false);
-			this.campoTextoCodigo.setText(puesto.getDescripcion());
+			this.campoTextoDescripcion.setText(puesto.getDescripcion());
 			this.campoTextoDescripcion.setDisable(false);
 		}//FIN METODO ELSE-IF
 	}//FIN METODO ACCESO A CLASE PRINCIPAL
@@ -72,40 +70,43 @@ public class DialogoPuesto {
 	//VALIDAR DATOS
 	private boolean validarDatos(){
 		if (campoTextoCodigo.getText().isEmpty()) {
-			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Código\" no puede estar vacio");
+			Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El campo \"Código\" no puede estar vacio");
 			return false;
-		}else if (campoTextoDescripcion.getText().isEmpty()) {
-			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Descripcion\" no puede estar vacio");
+		} else if (campoTextoDescripcion.getText().isEmpty()) {
+			Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El campo \"Descripcion\" no puede estar vacio");
 			return false;
 		}//FIN ELSE-IF
 		return true;
 	}// FIN METODO VALIR
 	
 	//MANEJADOR COMPONENTES
-	@FXML
-	private void manejadorBotonAceptar() {
-		if (validarDatos()&& opcion == CREAR) {
-			this.puesto.setCodigo(campoTextoCodigo.getText());
-			this.puesto.setDescripcion(campoTextoDescripcion.getText());
-			if (PuestoDAO.createPuesto(conexion, puesto)) {
-				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se creo correctamente");
-				this.mainApp.getEscenarioDialogos().close();
-			}else {
-				Notificacion.dialogoAlerta(AlertType.INFORMATION, "Error", "No se pudo crear el registro, revisa que la información sea correcta");
+	@FXML private void manejadorBotonAceptar() {
+		
+		if (validarDatos()) {
+			if (opcion == CREAR) {
+				this.puesto.setCodigo(campoTextoCodigo.getText());
+				this.puesto.setDescripcion(campoTextoDescripcion.getText());
+				if (PuestoDAO.createPuesto(conexion, puesto)) {
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se creo correctamente");
+					this.mainApp.getEscenarioDialogos().close();
+				}else {
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "Error", "No se pudo crear el registro, revisa que la información sea correcta");
+				}
+			}else if ( opcion == EDITAR) {
+				this.puesto.setCodigo(campoTextoCodigo.getText());
+				this.puesto.setDescripcion(campoTextoDescripcion.getText());
+				if (PuestoDAO.updatePuesto(conexion, puesto)) {
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se actualizo correctamente");
+					this.mainApp.getEscenarioDialogos().close();
+				}else {
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "Error", "No se pudo actualizar el registro, revisa que la informacion sea correcta");
+				}
+			} else if (opcion == VER) {
+				mainApp.getEscenarioDialogos().close();
 			}
-		}else if(validarDatos() && opcion == EDITAR) {
-			this.puesto.setCodigo(campoTextoCodigo.getText());
-			this.puesto.setDescripcion(campoTextoDescripcion.getText());
-			if (PuestoDAO.updatePuesto(conexion, puesto)) {
-				Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se actualizo correctamente");
-			}else {
-				Notificacion.dialogoAlerta(AlertType.INFORMATION, "Error", "No se pudo actualizar el registro, revisa que la informacion sea correcta");
-			}
-		}
-		else if (validarDatos() && opcion == VER) {
-			mainApp.getEscenarioDialogos().close();
-		}
-	}
+		}//FIN ELSE-IF
+	}//FIN MANEJADOR BOTON ACEPTAR
+
 	//BOTON CERRAR
 	@FXML 
 	private void manejadorBotonCerrar() {
