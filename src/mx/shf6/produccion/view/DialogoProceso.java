7d61eb342278a18b4be9d5814408fa7dc66d416b
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-06-17 09:37:01 -0500

    DialogoProceso Correción de métodos

--- src/mx/shf6/produccion/view/DialogoProceso.java
+++ src/mx/shf6/produccion/view/DialogoProceso.java
@@ -1,7 +1,7 @@
 package mx.shf6.produccion.view;
 
 
-import java.time.LocalDate;
+import java.sql.Date;
 import java.util.ArrayList;
 
 import javafx.collections.FXCollections;
@@ -13,11 +13,14 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import mx.shf6.produccion.MainApp;
 import mx.shf6.produccion.model.CentroTrabajo;
+import mx.shf6.produccion.model.Componente;
+import mx.shf6.produccion.model.Empleado;
 import mx.shf6.produccion.model.Proceso;
 import mx.shf6.produccion.model.dao.CentroTrabajoDAO;
 import mx.shf6.produccion.model.dao.ComponenteDAO;
 import mx.shf6.produccion.model.dao.EmpleadoDAO;
 import mx.shf6.produccion.model.dao.ProcesoDAO;
+import mx.shf6.produccion.utilities.AutoCompleteComboBoxListener;
 import mx.shf6.produccion.utilities.Notificacion;
 import mx.shf6.produccion.utilities.RestriccionTextField;
 
@@ -26,12 +29,15 @@ public class DialogoProceso {
 	//PROPIEDADES
 	private MainApp mainApp;
 	private Proceso proceso;
-	private CentroTrabajo ct;
-	
+	private CentroTrabajo centroTrabajo;
+	private Componente componente;
+	private Empleado empleado;
 	private ArrayList<CentroTrabajo> listaCentrosTrabajo;
-	private ObservableList<String> observableCT;
-	private ObservableList<String> listaComponentes;
-	private ObservableList<String> listaEmpleados;
+	private ArrayList<Componente> listaComponentes;
+	private ArrayList<Empleado> listaEmpleados;
+	private ObservableList<String> observableCentroTrabajo;
+	private ObservableList<String> observableComponentes;
+	private ObservableList<String> observableEmpleados;
 	
 	//VARIABLES
 	private int opcion;
@@ -50,38 +56,57 @@ public class DialogoProceso {
 	@FXML private ComboBox<String> comboTextoComponentes;
 	@FXML private ComboBox<String> comboBoxEmpleados;
 	
-	//INICIA COMPONENTES INTERFAZ USUARIO
-	@FXML private void initialize() {
+	//ACCESO A LA CLASE PRINCIPAL
+	public void setMainApp(MainApp mainApp, Proceso proceso, Integer opcion) {
+		this.mainApp = mainApp;
+		this.proceso = proceso;
+		this.opcion = opcion;
+		this.observableCentroTrabajo = FXCollections.observableArrayList();
+		this.observableComponentes = FXCollections.observableArrayList();
+		this.observableEmpleados = FXCollections.observableArrayList();
+		this.componente = ComponenteDAO.readComponente(this.mainApp.getConnection(), proceso.getComponenteFK());
+		this.centroTrabajo = CentroTrabajoDAO.readCentroTrabajo(this.mainApp.getConnection(), proceso.getCentroTrabajoFK());
+		this.empleado = EmpleadoDAO.readEmpleado(this.mainApp.getConnection(), proceso.getEmpleadoFK());
+		inicializarCombosCT();
+		this.inicializarComponentes();
+		restricciones();
+	}//FIN METODO
+	
+	//RESTRICCIONES
+	private void restricciones() {
 		RestriccionTextField.soloNumeros(this.campoTextoCantidad);
 		RestriccionTextField.limitarNumeroCaracteres(this.campoTextoCantidad, 4);
 		RestriccionTextField.soloNumeros(this.campoTextoOrdenamiento);
 		RestriccionTextField.limitarNumeroCaracteres(this.campoTextoOrdenamiento, 4);
 		RestriccionTextField.soloNumeros(this.campoTextoNivel);
 		RestriccionTextField.limitarNumeroCaracteres(this.campoTextoNivel, 2);
-		this.proceso = new Proceso();
 	}//FIN METODO
 	
-	//ACCESO CLASE PRINCIPAL
-	public void setMainApp(MainApp mainApp, Proceso proceso, Integer opcion, CentroTrabajo ct) {
-		this.mainApp = mainApp;
-		this.proceso = proceso;
-		this.opcion = opcion;
-		this.ct = new CentroTrabajo();
-		this.observableCT = FXCollections.observableArrayList();
-		this.inicializarComponentes();
-	}//FIN METODO
-	
+	//INICIALIZAR COMBOS
 	private void inicializarCombosCT() {
 		listaCentrosTrabajo = CentroTrabajoDAO.readCentroTrabajo(this.mainApp.getConnection());
 		for (CentroTrabajo trabajo : listaCentrosTrabajo) 
-			this.observableCT.add(trabajo.getDescripcion());
-		this.comboBoxDestino.setItems(this.observableCT);
-	}
+			this.observableCentroTrabajo.add(trabajo.getDescripcion());
+		this.comboBoxDestino.setItems(this.observableCentroTrabajo);
+		new AutoCompleteComboBoxListener(comboBoxDestino);
+		
+		listaComponentes = ComponenteDAO.readComponente(this.mainApp.getConnection());
+		for (Componente componente : listaComponentes)
+			this.observableComponentes.add(componente.getNumeroParte());
+		this.comboTextoComponentes.setItems(this.observableComponentes);
+		new AutoCompleteComboBoxListener(comboTextoComponentes);
+		
+		listaEmpleados = EmpleadoDAO.readEmpleado(this.mainApp.getConnection());
+		for (Empleado empleado : listaEmpleados)
+			this.observableEmpleados.add(empleado.getNombre());
+		this.comboBoxEmpleados.setItems(this.observableEmpleados);
+		new AutoCompleteComboBoxListener(comboBoxEmpleados);
+	}//FIN METODO
 	
 	//INICIALIZA COMPONENTE
 	private void inicializarComponentes() {
-		if(this.opcion == MOSTRAR) {
-			this.campoDateFecha.setUserData(this.proceso.getFecha());
+		if (this.opcion == MOSTRAR) {
+			this.campoDateFecha.setValue(this.proceso.getFecha().toLocalDate());
 			this.campoDateFecha.setDisable(true);
 			this.campoTextoCantidad.setText(this.proceso.getCantidad().toString());
 			this.campoTextoCantidad.setDisable(true);
@@ -89,11 +114,11 @@ public class DialogoProceso {
 			this.campoTextoOrdenamiento.setDisable(true);
 			this.campoTextoNivel.setText(this.proceso.getNivel().toString());
 			this.campoTextoNivel.setDisable(true);
-			this.comboBoxDestino.getSelectionModel().select(proceso.getCentroTrabajoFK());
+			this.comboBoxDestino.setValue(centroTrabajo.getDescripcion());
 			this.comboBoxDestino.setDisable(true);
-			this.comboTextoComponentes.getSelectionModel().select(proceso.getComponenteFK());
+			this.comboTextoComponentes.setValue(componente.getNumeroParte());
 			this.comboTextoComponentes.setDisable(true);
-			this.comboBoxEmpleados.getSelectionModel().select(proceso.getEmpleadoFK());
+			this.comboBoxEmpleados.setValue(empleado.getNombre());
 			this.comboBoxEmpleados.setDisable(true);
 		} else if (this.opcion == CREAR) {
 			this.campoDateFecha.setUserData("");
@@ -111,7 +136,7 @@ public class DialogoProceso {
 			this.comboBoxEmpleados.getSelectionModel().select("");
 			this.comboBoxEmpleados.setDisable(false);
 		} else if (this.opcion == EDITAR) {
-			this.campoDateFecha.setUserData(this.proceso.getFecha());
+			this.campoDateFecha.setValue(this.proceso.getFecha().toLocalDate());
 			this.campoDateFecha.setDisable(false);
 			this.campoTextoCantidad.setText(String.valueOf(proceso.getCantidad()));
 			this.campoTextoCantidad.setDisable(false);
@@ -119,36 +144,27 @@ public class DialogoProceso {
 			this.campoTextoOrdenamiento.setDisable(false);
 			this.campoTextoNivel.setText(String.valueOf(proceso.getNivel()));
 			this.campoTextoNivel.setDisable(false);
-			this.comboBoxDestino.getSelectionModel().select(proceso.getCentroTrabajoFK());
+			this.comboBoxDestino.setValue(centroTrabajo.getDescripcion());
 			this.comboBoxDestino.setDisable(false);
-			this.comboTextoComponentes.getSelectionModel().select(proceso.getComponenteFK());
+			this.comboTextoComponentes.setValue(componente.getNumeroParte());
 			this.comboTextoComponentes.setDisable(false);
-			this.comboBoxEmpleados.getSelectionModel().select(proceso.getEmpleadoFK());
+			this.comboBoxEmpleados.setValue(empleado.getNombre());;
 			this.comboBoxEmpleados.setDisable(false);
 		}//FIN IF-ELSE
 	}//FIN METODO
 	
 	//VALIDAR METODOS
 	private boolean validarDatos() {
-		if(this.comboBoxDestino.getSelectionModel().isEmpty()) {
-			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Destino \" no puede estar vacio");
-			return false;
-		} else if(this.comboBoxEmpleados.getSelectionModel().isEmpty()) {
-			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Empleado\" no puede estar vacio");
-			return false;
-		} else if(this.comboTextoComponentes.getSelectionModel().isEmpty()) {
-			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Componente \" no puede estar vacio");
-			return false;
-		} else if(this.campoDateFecha.getValue() == null) {
+		if (this.campoDateFecha.getValue() == null) {
 			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Fecha \" no puede estar vacio");
 			return false;
-		} else if(this.campoTextoCantidad.getText().isEmpty()) {
+		} else if (this.campoTextoCantidad.getText().isEmpty()) {
 			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Cantidad \" no puede estar vacio");
 			return false;
-		} else if(this.campoTextoNivel.getText().isEmpty()) {
+		} else if (this.campoTextoNivel.getText().isEmpty()) {
 			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Nivel \" no puede estar vacio");
 			return false;
-		} else if(this.campoTextoOrdenamiento.getText().isEmpty()) {
+		} else if (this.campoTextoOrdenamiento.getText().isEmpty()) {
 			Notificacion.dialogoAlerta(AlertType.ERROR, "", "El campo \"Orden \" no puede estar vacio");
 			return false;
 		}//FIN IF-ELSE
@@ -157,27 +173,46 @@ public class DialogoProceso {
 	
 	//MANEJADORES COMPONENTES
 	@FXML private void manejadorBotonAceptar() {
-		if(this.validarDatos()) {
-			if(this.opcion == CREAR) {
-				LocalDate 
-				this.proceso.setFecha(this.campoDateFecha.d);
+		if (this.validarDatos()) {
+			if (this.opcion == CREAR) {
+				this.proceso.setFecha(Date.valueOf(this.campoDateFecha.getValue()));
 				this.proceso.setCantidad(Integer.parseInt(this.campoTextoCantidad.getText()));
 				this.proceso.setOrdenamiento(Integer.parseInt(this.campoTextoOrdenamiento.getText()));
 				this.proceso.setNivel(Integer.parseInt(this.campoTextoNivel.getText()));
-				this.proceso.setCentroTrabajoFK(this.comboBoxDestino.getSelectionModel().getSelectedItem());
-				this.proceso.setComponenteFK(this.comboTextoComponentes.getSelectionModel().getSelectedItem());
-				this.proceso.setEmpleadoFK(this.comboBoxEmpleados.getSelectionModel().getSelectedItem());
+				this.proceso.setCentroTrabajoFK(listaCentrosTrabajo.get(comboBoxDestino.getSelectionModel().getSelectedIndex()).getSysPK());
+				this.proceso.setComponenteFK(listaComponentes.get(comboTextoComponentes.getSelectionModel().getSelectedIndex()).getSysPK());
+				this.proceso.setEmpleadoFK(listaEmpleados.get(comboBoxEmpleados.getSelectionModel().getSelectedIndex()).getSysPK());
 				
 				if (ProcesoDAO.createProceso(this.mainApp.getConnection(), this.proceso)) {
+					manejadorBotonCerrar();
 					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se creo de forma correcta");
 				} else {
-					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "No se pudo crear el registro, revisa que la información sea correcta");
-				}
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "No se pudo crear el registro, revisa que la información sea correcta.");
+				}//FIN IF-ELSE
 				
 			} else if (this.opcion == EDITAR) {
-				this.proceso.setFecha(fecha);
-			}
-		}//FIN
-	}//FIN METODO 
+				this.proceso.setFecha(Date.valueOf(this.campoDateFecha.getValue()));
+				this.proceso.setCantidad(Integer.parseInt(this.campoTextoCantidad.getText()));
+				this.proceso.setOrdenamiento(Integer.parseInt(this.campoTextoOrdenamiento.getText()));
+				this.proceso.setNivel(Integer.parseInt(this.campoTextoNivel.getText()));
+				this.proceso.setCentroTrabajoFK(listaCentrosTrabajo.get(comboBoxDestino.getSelectionModel().getSelectedIndex()).getSysPK());
+				this.proceso.setComponenteFK(listaComponentes.get(comboTextoComponentes.getSelectionModel().getSelectedIndex()).getSysPK());
+				this.proceso.setEmpleadoFK(listaEmpleados.get(comboBoxEmpleados.getSelectionModel().getSelectedIndex()).getSysPK());
+				
+				if (ProcesoDAO.updateProceso(this.mainApp.getConnection(), this.proceso)) {
+					manejadorBotonCerrar();
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "El registro se modifico correctamente");
+				} else {
+					Notificacion.dialogoAlerta(AlertType.INFORMATION, "", "No se pudo modificar el registro, revisa la información sea correcta");
+				}//FIN IF-ELSE
+			} else if (this.opcion == MOSTRAR) {
+				manejadorBotonCerrar();
+			}//FIN IF-ELSE
+		}//FIN METODO
+	}//FIN METODO
+	
+	@FXML private void manejadorBotonCerrar() {
+		this.mainApp.getEscenarioDialogos().close();
+	}//FIN METODO
 	
 }//FIN CLASE
