25635b31fb139bf37ec1583b32c1a3f64596d1b8
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-07-01 09:37:17 -0500

    Se agrego la consulta faltante

--- src/mx/shf6/produccion/model/dao/OrdenProduccionDAO.java
+++ src/mx/shf6/produccion/model/dao/OrdenProduccionDAO.java
@@ -17,9 +17,7 @@ public class OrdenProduccionDAO {
 		String consulta = "INSERT INTO ordenesproduccion (Fecha, Lote, Status, DetalleCotizacionFK) VALUES (CURDATE(),?,0,?)";
 		try {
 			PreparedStatement sentenciaPreparada = connection.prepareStatement(consulta);
-			//sentenciaPreparada.setDate(1, ordenProduccion.getFecha());
 			sentenciaPreparada.setString(1, ordenProduccion.getLote());
-			//sentenciaPreparada.setInt(3, ordenProduccion.getStatus());
 			sentenciaPreparada.setInt(2, ordenProduccion.getDetalleCotizacionFK());
 			sentenciaPreparada.execute();
 			return true;
@@ -32,7 +30,7 @@ public class OrdenProduccionDAO {
 	//METODO PARA OBTENER UN REGISTRO
 	public static ArrayList<OrdenProduccion> readLoteProduccion(Connection connection) {
 		ArrayList<OrdenProduccion> arrayListaOrdenProduccion = new ArrayList<OrdenProduccion>();
-		String consulta = "SELECT";
+		String consulta = "SELECT Sys_PK, Fecha, Lote, Status, DetalleCotizacionFK FROM ordenesproduccion";
 		try {
 			Statement sentencia = connection.createStatement();
 			ResultSet resultados = sentencia.executeQuery(consulta);
@@ -51,6 +49,25 @@ public class OrdenProduccionDAO {
 		return arrayListaOrdenProduccion;
 	}//FIN METODO
 	
+	public static OrdenProduccion searchOrdenProduccion(Connection connection, int detalleCotizacionFK) {
+		OrdenProduccion ordenProduccion = new OrdenProduccion();
+		String consulta = " SELECT Sys_PK, Fecha, Lote, Status, DetalleCotizacionFK FROM ordenesproduccion WHERE DetalleCotizacionFK = " + detalleCotizacionFK;
+		try {
+			Statement sentencia = connection.createStatement();
+			ResultSet resultados = sentencia.executeQuery(consulta);
+			while (resultados.next()) {
+				ordenProduccion.setSysPK(resultados.getInt(1));
+				ordenProduccion.setFecha(resultados.getDate(2));
+				ordenProduccion.setLote(resultados.getString(3));
+				ordenProduccion.setStatus(resultados.getInt(4));
+				ordenProduccion.setDetalleCotizacionFK(resultados.getInt(5));
+			}//FIN WHILE
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+		}//FIN TRY CATCH
+		return ordenProduccion;
+	}//FIN METODO
+	
 	public static final int ultimoSysPK(Connection connection) {
 		String query = "SELECT Sys_PK FROM ordenesproduccion order by Sys_PK asc";
 		int ultimoSyspk = 0;
@@ -59,11 +76,12 @@ public class OrdenProduccionDAO {
 			ResultSet resultSet = statement.executeQuery(query);
 			while (resultSet.next()) {
 				ultimoSyspk = resultSet.getInt(1);
-			}
+			}//FIN WHILE
 		} catch (SQLException ex) {
 			System.out.println("No hay syspk " + ex);
-		}
+		}//FIN TRY CATCH
 		return ultimoSyspk;
-	}
+	}//FIN METODO
+	
 	
 }//FIN CLASE
