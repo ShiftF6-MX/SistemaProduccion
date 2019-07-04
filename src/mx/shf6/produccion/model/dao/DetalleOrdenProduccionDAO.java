06494ca80bd28d51d19fdda68cbfd4b71d7648e5
AntonioShiftF6 <antonio.garcia@gruposhift-f6.com.mx>
2019-07-04 10:55:49 -0500

    Se agregaron métodos para graficar

--- src/mx/shf6/produccion/model/dao/DetalleOrdenProduccionDAO.java
+++ src/mx/shf6/produccion/model/dao/DetalleOrdenProduccionDAO.java
@@ -47,5 +47,29 @@ public class DetalleOrdenProduccionDAO {
 		return listaDetalleLoteProduccion;
 	}//FIN METODO
 	
+	public static ArrayList<DetalleOrdenProduccion> readDetalleLoteProduccion(Connection connection, int orden) {
+		ArrayList<DetalleOrdenProduccion> listaDetalleLoteProduccion = new ArrayList<DetalleOrdenProduccion>();
+		String consulta = "";
+		if (orden < 0)
+			consulta = "SELECT Sys_PK, NumeroSerie, Status, OrdenProduccionFK FROM detalleordenesproduccion WHERE Status != 4";
+		else
+			consulta = "SELECT Sys_PK, NumeroSerie, Status, OrdenProduccionFK FROM detalleordenesproduccion  WHERE OrdenProduccionFK = " + orden + " AND Status != 4";
+		try {
+			Statement sentencia =connection.createStatement();
+			ResultSet resultados = sentencia.executeQuery(consulta);
+			while(resultados.next()) {
+				DetalleOrdenProduccion detalleLote = new DetalleOrdenProduccion();
+				detalleLote.setSysPK(resultados.getInt(1));
+				detalleLote.setNumeroSerie(resultados.getString(2));
+				detalleLote.setStatus(resultados.getInt(3));
+				detalleLote.setOrdenProduccionFK(resultados.getInt(4));
+				listaDetalleLoteProduccion.add(detalleLote);
+			}//FIN WHILE
+		} catch (SQLException ex) {
+			Notificacion.dialogoException(ex);
+		}//FIN TRY CATCH
+		return listaDetalleLoteProduccion;
+	}//FIN METODO
+	
 	
 }//FIN CLASE
