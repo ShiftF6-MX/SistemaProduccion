1db674a55756677dc54cf76d43e89f8627ecc208
karina <karina.reyes@gruposhift-f6.com.mx>
2019-08-07 11:04:39 -0500

    Se actulizan el modelo y DAO para agregar la propiedad EmpleadoFK

--- src/mx/shf6/produccion/model/Usuario.java
+++ src/mx/shf6/produccion/model/Usuario.java
@@ -8,10 +8,11 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.image.Image;
+import mx.shf6.produccion.model.dao.EmpleadoDAO;
 import mx.shf6.produccion.model.dao.GrupoUsuarioDAO;
 
 public class Usuario {
-	
+
 	//PROPIEDADES
 	private ObjectProperty<Integer> sysPk;
 	private StringProperty usuario;
@@ -23,20 +24,21 @@ public class Usuario {
 	private ObjectProperty<Integer> grupoUsuarioFk;
 	private ObjectProperty<Image> imagenPerfil;
 	private StringProperty nombreGrupoUsuario;
-	
+	private ObjectProperty<Integer> empleadoFK;
+
 	//CONSTANTES
 	public static final int BLOQUEADO = 0;
 	public static final int ACTIVO = 1;
 	public static final int BAJA = 2;
-	
-	//CONSTRUCTOR SIN PARAMETROS	
+
+	//CONSTRUCTOR SIN PARAMETROS
 	public Usuario() {
-		this(0,"","","",new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()),0,0,null, "");
+		this(0,"","","",new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()),0,0,null, "", 0);
 	}//FIN CONSTRUCTOR
-	
+
 	//CONSTRUCTOR CON PARAMETROS
 	public Usuario(Integer sysPk, String usuario, String contrasena, String correoElectronico, Date fechaRegistro, Date fechaBloqueo,
-			Integer status, Integer grupoUsuarioFk, Image imagenPerfil, String nombreGrupoUsuario) {
+			Integer status, Integer grupoUsuarioFk, Image imagenPerfil, String nombreGrupoUsuario, int empleadoFK) {
 		this.sysPk = new SimpleObjectProperty<Integer>(sysPk);
 		this.usuario = new SimpleStringProperty(usuario);
 		this.contrasena = new SimpleStringProperty(contrasena);
@@ -47,111 +49,112 @@ public class Usuario {
 		this.grupoUsuarioFk = new SimpleObjectProperty<Integer>(grupoUsuarioFk);
 		this.imagenPerfil = new SimpleObjectProperty<Image>(imagenPerfil);
 		this.nombreGrupoUsuario = new SimpleStringProperty(nombreGrupoUsuario);
+		this.empleadoFK = new SimpleObjectProperty<Integer>(empleadoFK);
 	}//FIN CONSTRUCTOR
-	
+
 	//METODOS PARA ACCESO A "SYSPK"
 	public void setSysPk(Integer sysPk) {
 		this.sysPk.set(sysPk);
 	}//FIN METODO
-	
+
 	public Integer getSysPk() {
 		return this.sysPk.get();
 	}//FIN METODO
-	
+
 	public ObjectProperty<Integer> sysPkProperty() {
 		return this.sysPk;
 	}//FIN METODO
 	//FINT METODOS "SYSPK"
-	
+
 	//METODOS PARA ACCESO A "NOMBRE"
 	public void setUsuario(String usuario) {
 		this.usuario.set(usuario);
 	}//FIN METODO
-	
+
 	public String getUsuario() {
 		return this.usuario.get();
 	}//FIN METODO
-	
+
 	public StringProperty usuarioProperty() {
 		return this.usuario;
 	}//FIN METODO
 	//FIN METODOS "NOMBRE"
-	
+
 	//METODOS PARA ACCESO A "CONTRASEÑA"
 	public void setContrasena(String contrasena) {
 		this.contrasena.set(contrasena);
 	}//FIN METODO
-	
+
 	public String getContrasena() {
 		return this.contrasena.get();
 	}//FIN METODO
-	
+
 	public StringProperty contrasenaProperty() {
 		return this.contrasena;
 	}//FIN METODO
 	//FIN METODOS "CONTRASENA"
-	
+
 	//METODOS PARA ACCESO A "CORREO ELECTRONICO"
 	public void setCorreoElectronico(String correoElectronico) {
 		this.correoElectronico.set(correoElectronico);
 	}//FIN METODO
-	
+
 	public String getCorreoElectronico( ) {
 		return this.correoElectronico.get();
 	}//FIN METODO
-	
+
 	public StringProperty correoElectronicoProperty() {
 		return this.correoElectronico;
 	}//FIN METODO
 	//FIN METODOS "CORREO ELECTRONICO"
-	
+
 	//METODOS PARA ACCESO A "FECHA REGISTRO"
 	public void setFechaRegistro(Date fechaRegistro) {
 		this.fechaRegistro.set(fechaRegistro);
 	}//FIN METODO
-	
+
 	public Date getFechaRegistro() {
 		if (this.fechaRegistro.get() == null)
 			return new Date(System.currentTimeMillis());
 		else
 			return this.fechaRegistro.get();
 	}//FIN METODO
-	
+
 	public ObjectProperty<Date> fechaRegistroProperty() {
 		return this.fechaRegistro;
 	}//END METODO
 	//END METODOS "FECHA REGISTRO"
-	
+
 	//METODOS PARA ACCESO A "FECHA BLOQUEO"
 	public void setFechaBloqueo(Date fechaBloqueo) {
 		this.fechaBloqueo.set(fechaBloqueo);
 	}//FIN METODO
-	
+
 	public Date getFechaBloqueo() {
 		if (this.fechaBloqueo.get() == null)
 			return new Date(System.currentTimeMillis());
 		else
 			return this.fechaBloqueo.get();
 	}//FIN METODO
-	
+
 	public ObjectProperty<Date> fechaBloqueProperty() {
 		return this.fechaBloqueo;
 	}//FIN METODO
 	//FIN METODOS "FECHA BLOQUEO"
-	
+
 	//METODOS PARA ACCESO A "STATUS"
 	public void setStatus(Integer status) {
 		this.status.set(status);
 	}//FIN METODO
-	
+
 	public Integer getStatus() {
 		return this.status.get();
 	}//FIN METODO
-	
+
 	public ObjectProperty<Integer> statusProperty() {
 		return this.status;
 	}//FIN METODO
-	
+
 
 	public String getDescripcionStatus() {
 		switch (this.getStatus()) {
@@ -164,7 +167,7 @@ public class Usuario {
 		}//FIN WTITCH
 		return "";
 	}//FIN METODO
-	
+
 	public void setNumeroStatus(String status) {
 		switch (status) {
 		case "Bloqueado":
@@ -178,7 +181,7 @@ public class Usuario {
 			break;
 		}//FIN WTITCH
 	}//FIN METODO
-	
+
 	public StringProperty descripcionStatusProperty() {
 		switch (this.getStatus()) {
 		case 0:
@@ -191,35 +194,35 @@ public class Usuario {
 		return new SimpleStringProperty();
 	}//FIN METODO
 	//FIN METODO "STATUS"
-	
+
 	//METODOS PARA ACCESO A "GRUPO USUARIO"
 	public void setGrupoUsuarioFk(Integer grupoUsuarioFk) {
 		this.grupoUsuarioFk.set(grupoUsuarioFk);
 	}//FIN METODO
-	
+
 	public Integer getGrupoUsuarioFk() {
 		return this.grupoUsuarioFk.get();
 	}//FIN METODO
-	
+
 	public GrupoUsuario getGrupoUsuario(Connection connection) {
 		return GrupoUsuarioDAO.readPorSysPK(connection, getGrupoUsuarioFk());
 	}//FIN METODO
 	//FIN METODOS "GRUPO USUARIO"
-	
+
 	//METODOS PARA ACCESO A "IMAGEN PERFIL"
 	public void setImagenPerfil(Image imagenPerfil) {
 		this.imagenPerfil.set(imagenPerfil);
 	}//FIN METODO
-	
+
 	public Image getImagenPerfil() {
 		return this.imagenPerfil.get();
 	}//FIN METODO
-	
+
 	public ObjectProperty<Image> imagenPerfilProperty() {
 		return this.imagenPerfil;
 	}//FIN METODO
 	//FIN METODOS DE ACCESO
-	
+
 	public String showInformacion() {
 		String informacionUsuario = "INFORMACIÓN DEL USUARIO \nSysPk: " + this.getSysPk() + "\n"
 				+ "Nombre: " + this.getUsuario() + "\n"
@@ -235,12 +238,30 @@ public class Usuario {
 	public void setNombreGrupoUsuario (String nombreGrupo) {
 		this.nombreGrupoUsuario.set(nombreGrupo);
 	}//FIN METODO
-	
+
 	public String getNombreGrupoUsuario() {
 		return this.nombreGrupoUsuario.get();
 	}//FIN METODO
-	
+
 	public StringProperty nombreGrupoUsuarioProperty() {
 		return this.nombreGrupoUsuario;
 	}//FIN METODO
+
+	//METODOS PARA ACCESO A "EMPLEADOFK"
+	public void setEmpleadoFK(Integer empleadoFK) {
+		this.empleadoFK.set(empleadoFK);
+	}//FIN METODO
+
+	public Integer getEmpleadoFK() {
+		return this.empleadoFK.get();
+	}//FIN METODO
+
+	public ObjectProperty<Integer> empleadoFKProperty() {
+		return this.empleadoFK;
+	}//FIN METODO
+
+	public Empleado getEmpleado(Connection connection){
+		return EmpleadoDAO.readEmpleado(connection, getEmpleadoFK());
+	}//FIN METODO
+	//FINT METODOS "EMPLEADOFK"
 }//FIN CLASE
--- src/mx/shf6/produccion/model/dao/UsuarioDAO.java
+++ src/mx/shf6/produccion/model/dao/UsuarioDAO.java
@@ -14,15 +14,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 
 public class UsuarioDAO {
-	
+
 	//CONSTANTES
 	public static final int NO_REGISTRADO = 0;
 	public static final int CONRASENA_INCORRECTA = 1;
 	public static final int USUARIO_BLOQUEADO = 2;
 	public static final int ACCESO_CORRECTO = 3;
-	
+
 	//METODO PARA HACER CREATE EN LA TABLA USUARIOS
-	public static final boolean crear(Connection connection, Usuario usuario){	
+	public static final boolean crear(Connection connection, Usuario usuario){
 		String query=" INSERT INTO usuarios (Usuario, Contrasena, CorreoElectronico, FechaRegistro, FechaBloqueo, Status, GrupoUsuarioFK) VALUES ( ?, aes_encrypt(?, 'ShiftF6'), ?, CURDATE(), ?, ?, ?)";
 		try {
 			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
@@ -42,7 +42,7 @@ public class UsuarioDAO {
 			return false;
 		}//FIN TRY/CATCH
 	}//FIN METODO
-	
+
 	//METODO PARA LEER REGISTROS EN LA TABLA USUARIOS. EMMANUEL OSTRIA
 	public static final ArrayList<Usuario> readTodos(Connection connection){
 		ArrayList<Usuario> arrayListUsuario = new ArrayList<Usuario>();
@@ -51,7 +51,7 @@ public class UsuarioDAO {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
 			while (resultSet.next()) {
-				Usuario usuario = new Usuario();			
+				Usuario usuario = new Usuario();
 				usuario.setSysPk(resultSet.getInt(1));
 				usuario.setUsuario(resultSet.getString(2));
 				usuario.setContrasena(resultSet.getString(3));
@@ -68,7 +68,7 @@ public class UsuarioDAO {
 		}//FIN TRY/CATCH
 		return arrayListUsuario;
 	}//FIN METODO
-	
+
 	//METODO PARA OBTENER REGISTROS SEGUN SU USUARIO O CORREO
 	public static final ArrayList<Usuario> readPorUsuarioCorreoLike (Connection connection, String like){
 		ArrayList<Usuario> arrayListUsuario = new ArrayList<Usuario>();
@@ -78,7 +78,7 @@ public class UsuarioDAO {
 			ResultSet resultSet = statement.executeQuery(query);
 			Usuario usuario = null;
 			while (resultSet.next()) {
-				usuario = new Usuario();		
+				usuario = new Usuario();
 				usuario.setSysPk(Integer.parseInt(resultSet.getString(1)));
 				usuario.setUsuario(resultSet.getString(2));;
 				usuario.setContrasena(resultSet.getString(3));
@@ -94,18 +94,18 @@ public class UsuarioDAO {
 		}//FIN TRY/CATCH
 		return arrayListUsuario;
 	}//FIN METODO
-	
+
 	//METODO PARA HACER SELECT EN LA TABLA USUARIOS
 	public static final ArrayList<Usuario> readPorCampo(Connection connection, String campoBusqueda, String valorBusqueda) {
 		ArrayList<Usuario> arrayListUsuario = new ArrayList<Usuario>();
-		String query = "SELECT Sys_PK, Usuario, aes_decrypt(Contrasena, 'ShiftF6'), CorreoElectronico, FechaRegistro, FechaBloqueo, Status, GrupoUsuarioFK FROM usuarios WHERE "+campoBusqueda+" = ? ORDER BY Sys_PK";
+		String query = "SELECT Sys_PK, Usuario, aes_decrypt(Contrasena, 'ShiftF6'), CorreoElectronico, FechaRegistro, FechaBloqueo, Status, GrupoUsuarioFK, EmpleadoFK FROM usuarios WHERE "+campoBusqueda+" = ? ORDER BY Sys_PK";
 			try {
 				PreparedStatement preparedStatement = connection.prepareStatement(query);
 				preparedStatement.setString(1, valorBusqueda);
 				ResultSet resultSet=preparedStatement.executeQuery();
 				Usuario usuario = null;
 				while (resultSet.next()) {
-					usuario = new Usuario();		
+					usuario = new Usuario();
 					usuario.setSysPk(Integer.parseInt(resultSet.getString(1)));
 					usuario.setUsuario(resultSet.getString(2));;
 					usuario.setContrasena(resultSet.getString(3));
@@ -114,19 +114,20 @@ public class UsuarioDAO {
 					usuario.setFechaBloqueo(resultSet.getDate(6));
 					usuario.setStatus(resultSet.getInt(7));
 			    	usuario.setGrupoUsuarioFk(resultSet.getInt(8));
+			    	usuario.setEmpleadoFK(resultSet.getInt(9));
 					arrayListUsuario.add(usuario);
 				}//END WHILE
 			} catch (SQLException ex) {
 				Notificacion.dialogoException(ex);
-			}//FIN TRy/CATCH	
+			}//FIN TRy/CATCH
 		return arrayListUsuario;
-	}//FIN METODO	
-	
-	
+	}//FIN METODO
+
+
 	//METODO PARA ACTUALIZAR UN REGITRO
 	public static final boolean update(Connection connection, Usuario usuario){
-		String query = "UPDATE usuarios SET Usuario = ?, Contrasena = aes_encrypt(?, 'ShiftF6'), CorreoElectronico = ?, FechaBloqueo = ?, Status = ?, GrupoUsuarioFK = ? WHERE Sys_PK = ?";		
-		try {					
+		String query = "UPDATE usuarios SET Usuario = ?, Contrasena = aes_encrypt(?, 'ShiftF6'), CorreoElectronico = ?, FechaBloqueo = ?, Status = ?, GrupoUsuarioFK = ? WHERE Sys_PK = ?";
+		try {
 			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
 			preparedStatement.setString(1, usuario.getUsuario());
 			preparedStatement.setString(2, usuario.getContrasena());
@@ -145,12 +146,12 @@ public class UsuarioDAO {
 			Notificacion.dialogoException(ex);
 			return false;
 		}//FIN TRY/CATCH
-	}//FIN METODO	
+	}//FIN METODO
 
 	//METODO PARA HACER DELETE EN LA TABLA USUARIOS
 	public static final boolean eliminar(Connection connection, Usuario usuario) {
 		String query = "DELETE FROM usuarios WHERE Sys_PK= ?";
-		try {	
+		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, usuario.getSysPk());
 			preparedStatement.execute();
@@ -158,23 +159,23 @@ public class UsuarioDAO {
 		} catch (SQLException ex) {
 			Notificacion.dialogoException(ex);
 			return false;
-		}//FIN TRY/CATCH		
+		}//FIN TRY/CATCH
 	}//FIN METODO
-	
-	
+
+
 	/*//METODO PARA CONVERTIR UNA ARRAYLIST A OBSERVABLELIST
 	public ObservableList<Usuario> toObservableList(ArrayList<Object> arrayList) {
-		ObservableList<Usuario> usuarioData = FXCollections.observableArrayList(); 
+		ObservableList<Usuario> usuarioData = FXCollections.observableArrayList();
 		for(Object usuario : arrayList) {
 			usuarioData.add((Usuario) usuario);
 		}//FIN FOR
 		return usuarioData;
 	}//FIN METODO*/
-	
-	
-	//METODO PARA VALIDAR SI UN USUARIO ESTA REGISTRADO Y/O BLOQUEDO Y SI HA ESCRITO CORRECTAMENTE SU CONTRASEÑA 
+
+
+	//METODO PARA VALIDAR SI UN USUARIO ESTA REGISTRADO Y/O BLOQUEDO Y SI HA ESCRITO CORRECTAMENTE SU CONTRASEÑA
 	public static final int validarUsuario(Connection connection, String nombreUsuario, String contrasena) {
-		Usuario usuario = new Usuario();		
+		Usuario usuario = new Usuario();
 		ArrayList<Usuario> resultadoUsuario = readPorCampo(connection, "usuario", nombreUsuario);
 		if (resultadoUsuario.size() != 0) {
 			usuario = (Usuario) resultadoUsuario.get(0);
@@ -184,7 +185,7 @@ public class UsuarioDAO {
 						return USUARIO_BLOQUEADO;//USUARIO BLOQUEADO
 					}else {
 						return ACCESO_CORRECTO;//ACCESO CORRECTO
-					}//FIN IF-ELSE		
+					}//FIN IF-ELSE
 				}else {
 					return CONRASENA_INCORRECTA;//CONTRASENA INCORRECTA
 				}//FIN IF-ELSE
@@ -192,9 +193,9 @@ public class UsuarioDAO {
 		}else {
 			return NO_REGISTRADO;//USUARIO NO REGISTRADO
 		}//FIN IF-ELSE
-		return 0;				
+		return 0;
 	}//FIN METODO
-	
+
 	//METDODO PARA OBTENER EL ULTIMO SYSPK REGISTRADO EN LA TABAL USUARIOS
 	public static final int ultimoSysPk(Connection connection) {
 		String query="SELECT Sys_PK FROM usuarios order by Sys_PK asc";
@@ -212,7 +213,7 @@ public class UsuarioDAO {
 		}//FIN TRY-CATCH
 		return ultimoSysPk;
 	}//FIN METODO
-	
+
 	public static final String aes_encrypt(Connection connection, String palabra) {
     	String query = "SELECT AES_ENCRYPT('" + palabra + "','ShiftF6')";
     	String desencriptacion = "";
@@ -226,9 +227,9 @@ public class UsuarioDAO {
 		}catch (SQLException ex) {
 			Notificacion.dialogoException(ex);
 			return desencriptacion;
-		}//FIN TRY-CATCH		
+		}//FIN TRY-CATCH
     }//FIN METODO
-	
+
 	public static final String aes_decrypt(Connection connection, String palabra) {
     	String query = "SELECT AES_DECRYPT('" + palabra + "','ShiftF6')";
     	String desencriptacion = "";
@@ -242,7 +243,7 @@ public class UsuarioDAO {
 		}catch (SQLException ex) {
 			Notificacion.dialogoException(ex);
 			return desencriptacion;
-		}//FIN TRY-CATCH			
+		}//FIN TRY-CATCH
     }//FIN METODO
 
 }//FIN CLASE
