af167517aab8e050239c11feea05d64f3533cd35
Emmanuel Ostria <emmanuel.ostria@gruposhift-f6.com.mx>
2019-08-08 16:19:28 -0500

    Clase Dimensiones Actualizada
    se agregan nuevas propiedades a la clase dimensiones.

--- src/mx/shf6/produccion/utilities/Dimensiones.java
+++ src/mx/shf6/produccion/utilities/Dimensiones.java
@@ -2,43 +2,112 @@ package mx.shf6.produccion.utilities;
 
 public class Dimensiones {
 	
-	//PROPIEDADES
 	private Double largo;
 	private Double ancho;
-	private Double altoEspesor;
+	private Double alto;
+	private Double diametroInterior;
+	private Double diametroExterior; //DIAMETRO SI LA PIEZA NO ES HUECA
+	private Double espesor;
+	private Double alto2;
+	private Double anchoTotal; //ANCHO + ANCHO2 + DIAMETRO
+	private String codigoCatalogo; //CODIGO DEL CATALGO PERFIL CORTINA
 	
-	public Dimensiones() {
-		this(0.0, 0.0, 0.0);
+	//CONSTRUCTOR VACIO
+	public Dimensiones (){
+		this(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, "");
 	}//FIN CONSTRUCTOR
-	
-	public Dimensiones(Double largo, Double ancho, Double altoEspesor) {
+
+	//CONSTRUCTOR
+	public Dimensiones(double largo, double ancho, double alto, double diametroInterior, double diametroExterior, double espesor, double alto2, double anchoTotal, String codigoCatalogo) {
 		this.largo = largo;
 		this.ancho = ancho;
-		this.altoEspesor = altoEspesor;
+		this.alto = alto;
+		this.diametroInterior = diametroInterior;
+		this.diametroExterior = diametroExterior;
+		this.espesor = espesor;
+		this.alto2 = alto2;
+		this.anchoTotal = anchoTotal;
+		this.codigoCatalogo = codigoCatalogo;
 	}//FIN CONSTRUCTOR
-
-	public Double getLargo() {
-		return largo;
-	}//FIN METODO
-
+	
+	//METODOS PARA ACCEDER A LARGO
 	public void setLargo(Double largo) {
 		this.largo = largo;
 	}//FIN METODO
-
-	public Double getAncho() {
-		return ancho;
+	
+	public Double getLargo() {
+		return this.largo;
 	}//FIN METODO
-
+	
+	//METODOS PARA ACCEDER A ANCHO
 	public void setAncho(Double ancho) {
 		this.ancho = ancho;
 	}//FIN METODO
-
-	public Double getAltoEspesor() {
-		return altoEspesor;
-	}//FIN METODO
-
-	public void setAltoEspesor(Double altoEspesor) {
-		this.altoEspesor = altoEspesor;
+	
+	public Double getAncho() {
+		return this.ancho;
 	}//FIN METODO
 	
+	//METODOS PARA ACCEDER A ALTO
+	public void setAlto(Double alto) {
+		this.alto = alto;
+	}//FIN METODO
+	
+	public Double getAlto() {
+		return this.alto;
+	}//FIN METODO
+	
+	//METODOS PARA ACCEDER A DIAMETRO INTERIOR
+	public void setDiametroInterior(Double diametroInterior) {
+		this.diametroInterior = diametroInterior;
+	}///FIN METODO
+	
+	public Double getDiametroInterior() {
+		return this.diametroInterior;
+	}//FIN METODO
+	
+	//METODOS PARA ACCEDER A DIAMETRO EXTERIOR
+	public void setDiametroExterior(Double diametroExterior) {
+		this.diametroExterior = diametroExterior;
+	}//FIN METODO
+	
+	public Double getDiametroExterior() {
+		return this.diametroExterior;
+	}//FIN METODO
+	
+	//METODOS PARA ACCEDER A ESPESOR
+	public void setEspesor(Double espesor) {
+		this.espesor = espesor;
+	}//FIN METODO
+	
+	public Double getEspesor() {
+		return this.espesor;
+	}//FIN METODO
+	
+	//METODO PARA ACCEDER A ALTO2
+	public void setAlto2(Double alto2) {
+		this.alto2 = alto2;
+	}//FIN METODO
+	
+	public Double getAlto2() {
+		return this.alto2;
+	}//FIN METODO
+	
+	//METODO PARA ACCEDER A ANCHO TOTAL
+	public void setAnchoTotal(Double anchoTotal) {
+		this.anchoTotal = anchoTotal;
+	}//FIN METODO
+	
+	public Double getAnchoTotal() {
+		return this.anchoTotal;
+	}//FIN METODO
+	
+	//METODO PARA ACCEDER CODIGO CATALOGO	
+	public void setCodigoCatalogo(String codigoCatalogo) {
+		this.codigoCatalogo = codigoCatalogo;
+	}//FIN METODO
+	
+	public String getCodigoCatalogo() {
+		return this.codigoCatalogo;
+	}//FIN METODO
 }//FIN CLASE
