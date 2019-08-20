31cc4dd87fc40b4ebf680c2f02e95bde722a7fb2
Rago1995 <raul.gomez@gruposhift-f6.com.mx>
2019-08-20 11:50:09 -0500

    Generar Documento
    Metodo hojaProceso

--- src/mx/shf6/produccion/utilities/GenerarDocumento.java
+++ src/mx/shf6/produccion/utilities/GenerarDocumento.java
@@ -1,7 +1,9 @@
 package mx.shf6.produccion.utilities;
 
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,10 +12,14 @@ import mx.shf6.produccion.model.Cotizacion;
 import mx.shf6.produccion.model.DetalleCardex;
 import mx.shf6.produccion.model.DetalleComponente;
 import mx.shf6.produccion.model.DetalleCotizacion;
+import mx.shf6.produccion.model.DetalleProceso;
 import mx.shf6.produccion.model.Empleado;
+import mx.shf6.produccion.model.Proceso;
 import mx.shf6.produccion.model.Usuario;
 import mx.shf6.produccion.model.dao.DetalleCotizacionDAO;
+import mx.shf6.produccion.model.dao.DetalleProcesoDAO;
 import mx.shf6.produccion.model.dao.EmpleadoDAO;
+import mx.shf6.produccion.model.dao.ProcesoDAO;
 import mx.shf6.produccion.view.DialogoMovimientoInventario;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -27,6 +33,7 @@ import net.sf.jasperreports.view.JasperViewer;
 public class GenerarDocumento {
 	//PROPIEDADES
 	private static List<DetalleCotizacion> datosTabla;
+	private static List<DetalleProceso> tablaDetalleProceso;
 
 	public static void generaCotizacion(Connection connection, Cotizacion cotizacion, Usuario usuario) {
 		try {
@@ -70,7 +77,50 @@ public class GenerarDocumento {
 		}//TRY/CATH
 	}//END METHOD
 
+	
+	public static void generarHojaProceso(Connection conexion, int sysPK) {
+		
 
+		try {
+			Proceso procesito = new Proceso();
+			procesito = ProcesoDAO.readProcesoHoja(conexion, sysPK);
+			
+			JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile("resources/hojaProceso.jasper");
+			tablaDetalleProceso = DetalleProcesoDAO.toList(DetalleProcesoDAO.readDetalleProcesoFK(conexion, sysPK));
+			JRBeanCollectionDataSource itemsTabla = new JRBeanCollectionDataSource(tablaDetalleProceso);
+			Map<String,Object> parameters = new HashMap<String,Object>();
+			parameters.put("pNoParte", procesito.getNombreComponente());
+			parameters.put("pNombre", procesito.getDescripcionComponente());
+			parameters.put("pCliente", procesito.getNombreCliente());
+			parameters.put("pFecha", procesito.getFecha().toString());
+			if (procesito.getCantidad()==0) 
+				parameters.put("pCantidad", " ");
+			else
+				parameters.put("pcantidad", procesito.getCantidad());
+				
+			if (procesito.getOrdenamiento() == 0)
+				parameters.put("pOrdenamiento", " ");
+			else 
+				parameters.put("pOrdenamiento", procesito.getOrdenamiento());
+			parameters.put("pNivel", procesito.getNivel().toString());
+			parameters.put("pDestino", procesito.getNombreTrabajo());
+			parameters.put("Parameter1", itemsTabla);
+			
+			Date fecha = new Date();
+			 SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
+			 String fechaActual = dt1.format(fecha);
+			 parameters.put("pFechaActual", fechaActual);
+			
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+			JasperViewer jasperView = new JasperViewer(jasperPrint, false);
+			jasperView.setVisible(true);
+		} catch (JRException e) {
+			System.out.println("Error en el reporte Chidooo!! " + e);
+		}
+		
+		
+	}
+	
 	public static void generaValeMovimientoInventario(Connection connection, ArrayList <DetalleCardex> listaDetalleCardex, int tipoMovimiento) {
 		String titulo = "";
 		String referencia = "";
